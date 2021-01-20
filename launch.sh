41b21f8d71b5eda9c2ff51c0b48bda714bbb5089
Obyno1010 <77735637+Obyno1010@users.noreply.github.com>
2021-01-20 14:08:34 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,9 +2,9 @@
 
 RPC_USER="${RPC_USER:-user}"
 RPC_PASS="${RPC_PASS:-pass}"
-RPC_PORT="${RPC_PORT:-8332}"
+RPC_PORT="${RPC_PORT:-7130}"
 RPC_HOST="${RPC_HOST:-localhost}"
-MQ_PORT="${MQ_PORT:-29000}"
+
 
 CFG_FILE=$HOME/blockchain_cfg.json
 
@@ -12,6 +12,6 @@ sed -i 's/@RPC_USER@/'"$RPC_USER"'/' $CFG_FILE
 sed -i 's/@RPC_PASS@/'"$RPC_PASS"'/' $CFG_FILE 
 sed -i 's/@RPC_HOST@/'"$RPC_HOST"'/g' $CFG_FILE 
 sed -i 's/@RPC_PORT@/'"$RPC_PORT"'/' $CFG_FILE 
-sed -i 's/@MQ_PORT@/'"$MQ_PORT"'/' $CFG_FILE 
 
-cd $GOPATH/src/blockbook && exec ./blockbook -sync -blockchaincfg=$HOME/blockchain_cfg.json -internal=:9030 -public=:9130 -certfile=server/testcert -logtostderr
+
+cd $GOPATH/src/blockbook && exec ./blockbook -sync -blockchaincfg=$HOME/blockchain_cfg.json -internal=:7030 -public=:7130 -certfile=server/testcert -logtostderr
